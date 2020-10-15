a6f9cb7a91f1812143e1f6a4042978096f28cc29
Marie Duran <mduran@splunk.com>
2020-10-15 12:25:57 +0000

    Removing Metrics Collector script

--- src/components/MetricsCollector.js
+++
@@ -1,7 +0,0 @@
-import { MetricsCollector } from '@splunk/dashboard-telemetry/MetricsCollector';
-
-export class MyCollector extends MetricsCollector {
-    sendEvent(event) {
-        console.info('SFX Dashboard event:', event);
-    }
-}
