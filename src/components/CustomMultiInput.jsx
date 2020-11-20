cb0bd43b717ad00143a61d664af61b72d9ab010d
damola <dshomoye@gmail.com>
2020-11-20 09:49:08 -0800

    add LICENSE file and license to JS files.

--- src/components/CustomMultiInput.jsx
+++ src/components/CustomMultiInput.jsx
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2020 Splunk Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @license Apache-2.0
+ */
+
 import React, {useState, useEffect, useMemo, useCallback }  from 'react';
 import isEmpty from 'lodash/isEmpty';
 import Multiselect from '@splunk/react-ui/Multiselect';
@@ -29,7 +46,7 @@ const CustomMultiInput = ({
                 setStatusMessage("Error occured with dataSource");
                 return true
             }
-            
+
             if(dataSources.primary.data.columns.length === 0){
                 setStatusMessage(dataSources.primary.meta.statusMessage);
                 return true
@@ -39,12 +56,12 @@ const CustomMultiInput = ({
                 const primary = dataSources.primary.data.columns;
                 const label = eval(encoding.label)
                 const value = eval(encoding.value)
-                
+
                 if(label === undefined || value === undefined){
                     setStatusMessage("Error occured in encoding, ensure proper format (e.g. primary[0])");
                     return true
                 }
-                
+
                 return label.map((item,idx) => (
                     <Multiselect.Option label={item} value={value[idx]} />
                 ));
@@ -71,14 +88,14 @@ const CustomMultiInput = ({
         <div>
         {!multiselectOptions && <div><WaitSpinner size="medium" screenReaderText={loadingMessage} /> </div>}
         {multiselectOptions &&
-            <Multiselect 
-                values={values} 
-                inline 
-                compact 
-                onChange={handleValueChange} 
-                placeholder={placeholder} 
-                isLoadingOptions={isLoading} 
-                loadingMessage={loadingMessage} 
+            <Multiselect
+                values={values}
+                inline
+                compact
+                onChange={handleValueChange}
+                placeholder={placeholder}
+                isLoadingOptions={isLoading}
+                loadingMessage={loadingMessage}
                 disabled={isDisabled || isError}
                 footerMessage={searchStatusMessage}
             >
